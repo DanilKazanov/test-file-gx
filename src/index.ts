1a58d146c40a4f41b168d77a88750e36c3f54a27
sinisterSensation <bayukov.kirya@mail.ru>
2024-03-19 10:35:09 +0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
 import FileSystemProvider from "./components/FileSystemProvider";
 import CustomZipper from "./components/CustomZipper";
-import fs from 'fs';
+import fs from "fs";
 
 interface SomeZipper {
-    zip(pathToDir: string, fp: FileSystemProvider): Promise<Buffer>;
-    unzip(pathToUnzip: string, content: any, fp: FileSystemProvider): Promise<void>;
-  }
+	zip(pathToDir: string, fp: FileSystemProvider): Promise<Buffer>;
+	unzip(pathToUnzip: string, content: Buffer, fp: FileSystemProvider): Promise<void>;
+}
 
 const fileProvider = new FileSystemProvider();
 const zipper = new CustomZipper();
 
 const runZip = async () => {
-  const pathToDirectory = 'C:/Users/User1/Desktop/arch/toZipFolder';
-  const zippedContent = await zipper.zip(pathToDirectory, fileProvider);
-  await fs.promises.writeFile('C:/Users/User1/Desktop/arch/toZip/Zip.zip', zippedContent);
-  console.log("Архивация завершена, архив сохранён.");
+	const pathToDirectory = "C:/Users/User1/Desktop/arch/toZipFolder";
+	const zippedContent = await zipper.zip(pathToDirectory, fileProvider);
+	await fs.promises.writeFile("C:/Users/User1/Desktop/arch/toZip/Zip.zip", zippedContent);
+	console.log("Архивация завершена, архив сохранён.");
 };
 
 const runUnzip = async () => {
-  const pathToUnzip = 'C:/Users/User1/Desktop/arch/toUnzipZip.zip';
-  const contentToUnzip = 'C:/Users/User1/Desktop/arch/toUnzip';
-  await zipper.unzip(pathToUnzip, contentToUnzip, fileProvider);
-  console.log("Разархивация завершена.");
+	const pathToUnzip = "";
+	const contentToUnzip = "C:/Users/User1/Desktop/arch/toUnzip";
+	await zipper.unzip(pathToUnzip, contentToUnzip, fileProvider);
+	console.log("Разархивация завершена.");
 };
 
 // runZip().catch(console.error);
-runUnzip().catch(console.error)
+runUnzip().catch(console.error);
 
 export default SomeZipper;
-
-
