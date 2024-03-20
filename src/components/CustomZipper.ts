3c5d8e284b3ad98ba8db3bfc7254aaf95586e7bb
sinisterSensation <bayukov.kirya@mail.ru>
2024-03-20 13:48:19 +0300

    zip unzip folders files

--- src/components/CustomZipper.ts
+++ src/components/CustomZipper.ts
@@ -4,26 +4,26 @@ import SomeZipper from "../models/models";
 import FileSystemProvider from "./FileSystemProvider";
 
 class CustomZipper implements SomeZipper {
-	async zip(pathToDir: string, fp: FileSystemProvider): Promise<Buffer> {
-		const zip = new JSZip();
-		const rootPath = path.resolve(pathToDir);
-
-		const addFiles = async (currentPath: string, zipPath: string) => {
-			const items = await fp.getItems(currentPath);
-			for (const item of items) {
-				const relativeFilePath = path.relative(rootPath, item.path);
-				const currentZipPath = path.join(zipPath, relativeFilePath);
-				if (item.isFile) {
-					const fileContent = await fp.readAsBinary(item.path);
-					zip.file(currentZipPath, fileContent);
-				} else {
-					await addFiles(item.path, currentZipPath);
-				}
-			}
-		};
-		await addFiles(rootPath, "");
-		return zip.generateAsync({ type: "nodebuffer" });
-	}
+    async zip(pathToDir: string, fp: FileSystemProvider): Promise<Buffer> {
+        const zip = new JSZip();
+        const rootPath = path.resolve(pathToDir);
+    
+        const addFiles = async (currentPath: string, zipPath: string) => {
+            const items = await fp.getItems(currentPath);
+            for (const item of items) {
+                const relativeFilePath = path.relative(rootPath, item.path);
+                const currentZipPath = path.join(zipPath, relativeFilePath);
+                if (item.isFile) {
+                    const fileContent = await fp.readAsBinary(item.path);
+                    zip.file(currentZipPath, fileContent);
+                } else {
+                    await addFiles(item.path, zipPath); 
+                }
+            }
+        };
+        await addFiles(rootPath, "");
+        return zip.generateAsync({ type: "nodebuffer" });
+    }
 
 	async unzip(zippedContent: Buffer, outputDirectory: string, fp: FileSystemProvider): Promise<void> {
 		const zip = await JSZip.loadAsync(zippedContent);
